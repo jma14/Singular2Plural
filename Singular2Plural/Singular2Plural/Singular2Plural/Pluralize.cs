a58de1cfc80c3217167d12a948b28923f65b3ca1
Jason <jarnoff14@comcast.net>
2017-03-13 16:34:53 -0400

    Singular2Plural
    Created console app for pluralizing a noun.  Validation for string
    length.  Checks special cases for plurals ending in 'es' or 'ies'.

---
+++ Singular2Plural/Singular2Plural/Singular2Plural/Pluralize.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singular2Plural
+{
+    public class Pluralize
+    {
+        private string _consonants = "bcdfghjklmnpqrstvwxz";
+        private List<string> _endingsForES = new List<string> { "ch", "s", "sh", "x", "z" };
+        private string _noun;
+        private char _last;
+        private char _secondToLast;
+        public string Plural;
+
+        public Pluralize(string noun)
+        {
+            _noun = noun.ToLower();  // Convert to lower case in order to make input case insensitive
+            _last = _noun[_noun.Length - 1]; // Get last letter of the noun
+            _secondToLast = _noun[_noun.Length - 2]; // Get second to last letter of the noun
+            Plural = checkNoun();
+        }
+
+        // Check different unique cases for pluralizing (i.e. not just adding an 's'), otherwise just add 's'
+        private string checkNoun()
+        {
+            if (needsIES())
+            {
+                return _noun.Substring(0, _noun.Length - 1) + "ies";
+            }
+            else if (needsES())
+            {
+                return _noun + "es";
+            }
+            else
+            {
+                return _noun + "s";
+            }
+        }
+
+        // Check for endings that require the plural to remove the 'y' and add 'ies'
+        private bool needsIES()
+        { 
+            if ((_consonants.IndexOf(_secondToLast) != -1) && (_last == 'y'))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Check for endings that require the plural to be the noun + 'es'
+        private bool needsES()
+        {
+            if(_endingsForES.Contains(_last.ToString()))
+            {
+                return true;
+            }
+            else if(_endingsForES.Contains(_secondToLast.ToString() + _last.ToString()))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Singular2Plural/Singular2Plural/Singular2Plural/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singular2Plural
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get user input noun and validate
+            string singularNoun;
+            bool isValid;
+            do
+            {
+                isValid = true;
+                
+                Console.WriteLine("Enter a noun: ");
+                singularNoun = Console.ReadLine();
+                if (singularNoun.Length < 2)
+                {
+                    isValid = false;
+                    Console.WriteLine("Invalid Input");
+                }
+            } while (isValid == false);
+           
+
+            //Pluralize the noun
+            Pluralize pluralize = new Pluralize(singularNoun);
+            string pluralNoun = pluralize.Plural;
+
+            //Print plural version of the noun to console
+            Console.WriteLine("The plural of {0} is {1}", singularNoun, pluralNoun);
+
+            //Have user press a key when they're ready to exit the program
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
